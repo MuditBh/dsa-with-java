27e6a524e8366dbbe481dd70d6166e6e8a0471dc
MUDIT BHARDWAJ <147296552+MuditBh@users.noreply.github.com>
2025-11-10 05:15:39 +0530

    Time: 3 ms (62.54%) | Memory: 43.4 MB (7.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean isValid(String s) 
     {
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
+        for (int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
             // If opening bracket, push to stack
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
