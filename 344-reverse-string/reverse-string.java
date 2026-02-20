3d460e429fd34fafefb86b91e7d58dc1cb443779
MUDIT BHARDWAJ <147296552+MuditBh@users.noreply.github.com>
2026-02-20 13:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (19.64%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,16 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        int start = 0;int end = s.length-1;
-        while(start<end)
+        int l = 0;
+        int r = s.length-1;
+        while(l<r)
         {
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            end--;start++;
-        }
-        for(int i=0;i<s.length;i++)
-        {
-            System.out.println(s[i]);
+            char temp = s[r];
+            s[r] = s[l];
+            s[l] = temp;
+            l++;
+            r--;
         }
         
     }
